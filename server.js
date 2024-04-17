80913367e9e36fa1969a42986019ebbd86fb2ac1
Muhammad Iqbal <muh.iqbalg1@gmail.com>
2024-04-17 13:47:23 +0700

    feat: first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.text({
+  type: function(req) {
+    return 'text';
+  }
+}));
+
+app.post('/post', function (req, res) {
+  console.log(req.body);
+  res = res.status(200);
+  res.send("Kamu Bilang: "+req.body);
+});
+
+http.createServer(app).listen(8000);
